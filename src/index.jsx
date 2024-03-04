eb400eb42a5f5542d2a152f402051000c4a9dc8a
imranmammadov12 <Mamed_qf28@itstep.edu.az>
2024-03-04 19:16:46 +0400

    deleting unnecessary comments

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,9 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-
-// index.js
-// index.js
 import './index.css';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { createBrowserRouter, RouterProvider, Routes} from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import TaskList from './TaskList';
 import store from './store';
-// // import TaskForm from './TaskForm';
-// import rootReducer from './rootReducers';
-// import AddTaskButton from './AddNewTask';
-// import NewTaskButton from './NewTaskButton';
 import Root, { loader as rootLoader, action as rootAction, } from './root';
 import Task, {loader as taskLoader} from './task';
 import EditTask, {action as editAction} from './edit';
